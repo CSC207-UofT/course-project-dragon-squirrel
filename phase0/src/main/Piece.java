23b14a88954ce2075e3a74283b78ef16d5a7f7ed
chrisphil335 <61081461+chrisphil335@users.noreply.github.com>
2021-10-12 13:10:07 -0400

    added alive attribute and isAlive method

--- phase0/src/main/Piece.java
+++ phase0/src/main/Piece.java
@@ -2,6 +2,7 @@ public class Piece {
     private String name;
     private Player owner;
     private boolean status;     // This tells whether this piece has been moved during the turn
+    private boolean alive = true;   // Whether the piece is alive or not (dead)
 
     public Piece(String name, Player owner) {
         status = false;
@@ -12,6 +13,10 @@ public class Piece {
     public Player getOwner() { return owner; }
 
     public boolean getStatus() { return status; }
+
+    public boolean isAlive() {
+        return this.alive;
+    }
 
     public boolean validMove(int oldCoorX, int oldCoorY, int newCoorX, int newCoorY) {
         if (newCoorX > 0 & newCoorX < 9 & newCoorY > 0 & newCoorY < 9 &
