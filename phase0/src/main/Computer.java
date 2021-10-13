3acc00d5a075f59f65587633c31a1bb851c1d518
chrisphil335 <61081461+chrisphil335@users.noreply.github.com>
2021-10-12 23:47:18 -0400

    changed 'AI' to 'Computer' for easier typing. adjusted instantiation to new parent design

---
+++ phase0/src/main/Computer.java
@@ -0,0 +1,10 @@
+// Designing a game (especially board game) AI could be difficult, maybe leave it until phase 2
+
+public class Computer extends Player{
+
+    public Computer(white){
+        this.white;
+        this.human = false;
+    }
+
+}
