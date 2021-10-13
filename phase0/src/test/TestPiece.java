49a5f8003d2ff1086717e0d7484cc865d8f470a2
chrisphil335 <61081461+chrisphil335@users.noreply.github.com>
2021-10-13 02:24:06 -0400

    creating test for Piece

--- phase0/src/test/TestPiece.java
+++ phase0/src/test/TestPiece.java
@@ -2,3 +2,35 @@ import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+
+public class PieceTest {
+    Piece p1;
+    Piece p2;
+    Piece p3;
+    Piece p4;
+
+    @Before
+    public void before(){
+        p1 = new Piece(true, true);
+        p2 = new Piece(true, false);
+        p3 = new Piece(false, true);
+        p4 = new Piece(false, false);
+    }
+
+    @Test(timeout = 50)
+    public void testIsWhite(){
+        assertEquals(true, p1.isWhite);
+        assertEquals(true, p2.isWhite);
+        assertEquals(false, p3.isWhite);
+        assertEquals(false, p4.isWhite);
+
+    }
+
+    @Test(timeout = 50)
+    public void testIsAlive(){
+        assertEquals(true, p1.isAlive);
+        assertEquals(false, p1.isAlive);
+        assertEquals(true, p1.isAlive);
+        assertEquals(false, p1.isAlive);
+    }
+}
