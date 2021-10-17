505b7ce3627a66e83c5fba5719e9bc6074283f0f
chrisphil335 <61081461+chrisphil335@users.noreply.github.com>
2021-10-16 21:05:12 -0400

    added Pawn test

--- phase0/src/test/TestPieces/TestPawn.java
+++ phase0/src/test/TestPieces/TestPawn.java
@@ -4,5 +4,18 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class PawnTest {
+    Pawn p;
+
+    @Before
+    public void setup(){
+        p = new Pawn("w_pawn", WHITE);
+    }
+
+    @Test
+    public void TestvalidMove(){
+        assertEquals(true, p.validMove(6, 0, 4, 0));
+        assertEquals(true, p.validMove(6, 0, 5, 0));
+        assertEquals(false, p.validMove(6, 0, 3, 0));
+    }
 
 }
