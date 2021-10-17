4cf689b843ab2de46f89888385b3e77254028de8
chrisphil335 <61081461+chrisphil335@users.noreply.github.com>
2021-10-16 20:09:04 -0400

    added AI test

--- phase0/src/test/TestAI.java
+++ phase0/src/test/TestAI.java
@@ -4,5 +4,11 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class AITest {
+    AI ai;
+
+    @Before
+    public void setup(){
+        ai = new AI("Computer Player");
+    }
 
 }
