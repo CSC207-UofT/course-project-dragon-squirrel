85556f60efb0f419ec36952c995e7e9a3a2869f1
chrisphil335 <61081461+chrisphil335@users.noreply.github.com>
2021-10-18 12:10:15 -0400

    changed piece id's to follow new naming scheme

--- phase0/src/test/TestBoard.java
+++ phase0/src/test/TestBoard.java
@@ -8,12 +8,12 @@ public class TestBoard {
     Board b;
 
     @Before
-    private void setup(){
+    public void setup(){
         b = new Board();
     }
 
     @After
-    private void after(){
+    public void after(){
         b.reset();
     }
 
@@ -22,40 +22,40 @@ public class TestBoard {
         String[][] board = new String[8][8];
 
         // initialize white pieces
-        board[7][0] = "w_rook";
-        board[7][1] = "w_knight";
-        board[7][2] = "w_bishop";
+        board[7][0] = "w_rook_l";
+        board[7][1] = "w_knight_l";
+        board[7][2] = "w_bishop_l";
         board[7][3] = "w_queen";
         board[7][4] = "w_king";
-        board[7][5] = "w_bishop";
-        board[7][6] = "w_knight";
-        board[7][7] = "w_rook";
-        board[6][0] = "w_pawn";
-        board[6][1] = "w_pawn";
-        board[6][2] = "w_pawn";
-        board[6][3] = "w_pawn";
-        board[6][4] = "w_pawn";
-        board[6][5] = "w_pawn";
-        board[6][6] = "w_pawn";
-        board[6][7] = "w_pawn";
+        board[7][5] = "w_bishop_r";
+        board[7][6] = "w_knight_r";
+        board[7][7] = "w_rook_r";
+        board[6][0] = "w_pawn_1";
+        board[6][1] = "w_pawn_2";
+        board[6][2] = "w_pawn_3";
+        board[6][3] = "w_pawn_4";
+        board[6][4] = "w_pawn_5";
+        board[6][5] = "w_pawn_6";
+        board[6][6] = "w_pawn_7";
+        board[6][7] = "w_pawn_8";
 
         // initialize black pieces
-        board[0][0] = "b_rook";
-        board[0][1] = "b_knight";
-        board[0][2] = "b_bishop";
+        board[0][0] = "b_rook_l";
+        board[0][1] = "b_knight_l";
+        board[0][2] = "b_bishop_l";
         board[0][3] = "b_queen";
         board[0][4] = "b_king";
-        board[0][5] = "b_bishop";
-        board[0][6] = "b_knight";
-        board[0][7] = "b_rook";
-        board[1][0] = "b_pawn";
-        board[1][1] = "b_pawn";
-        board[1][2] = "b_pawn";
-        board[1][3] = "b_pawn";
-        board[1][4] = "b_pawn";
-        board[1][5] = "b_pawn";
-        board[1][6] = "b_pawn";
-        board[1][7] = "b_pawn";
+        board[0][5] = "b_bishop_r";
+        board[0][6] = "b_knight_r";
+        board[0][7] = "b_rook_r";
+        board[1][0] = "b_pawn_1";
+        board[1][1] = "b_pawn_2";
+        board[1][2] = "b_pawn_3";
+        board[1][3] = "b_pawn_4";
+        board[1][4] = "b_pawn_5";
+        board[1][5] = "b_pawn_6";
+        board[1][6] = "b_pawn_7";
+        board[1][7] = "b_pawn_8";
 
         // initialize remaining board with no pieces
         for (int i = 2; i < 6; i++) {
@@ -97,40 +97,40 @@ public class TestBoard {
         String[][] board = new String[8][8];
 
         // initialize white pieces
-        board[7][0] = "w_rook";
-        board[7][1] = "w_knight";
-        board[7][2] = "w_bishop";
+        board[7][0] = "w_rook_l";
+        board[7][1] = "w_knight_l";
+        board[7][2] = "w_bishop_l";
         board[7][3] = "w_queen";
         board[7][4] = "w_king";
-        board[7][5] = "w_bishop";
-        board[7][6] = "w_knight";
-        board[7][7] = "w_rook";
-        board[6][0] = "w_pawn";
-        board[6][1] = "w_pawn";
-        board[6][2] = "w_pawn";
-        board[6][3] = "w_pawn";
-        board[6][4] = "w_pawn";
-        board[6][5] = "w_pawn";
-        board[6][6] = "w_pawn";
-        board[6][7] = "w_pawn";
+        board[7][5] = "w_bishop_r";
+        board[7][6] = "w_knight_r";
+        board[7][7] = "w_rook_r";
+        board[6][0] = "w_pawn_1";
+        board[6][1] = "w_pawn_2";
+        board[6][2] = "w_pawn_3";
+        board[6][3] = "w_pawn_4";
+        board[6][4] = "w_pawn_5";
+        board[6][5] = "w_pawn_6";
+        board[6][6] = "w_pawn_7";
+        board[6][7] = "w_pawn_8";
 
         // initialize black pieces
-        board[0][0] = "b_rook";
-        board[0][1] = "b_knight";
-        board[0][2] = "b_bishop";
+        board[0][0] = "b_rook_l";
+        board[0][1] = "b_knight_l";
+        board[0][2] = "b_bishop_l";
         board[0][3] = "b_queen";
         board[0][4] = "b_king";
-        board[0][5] = "b_bishop";
-        board[0][6] = "b_knight";
-        board[0][7] = "b_rook";
-        board[1][0] = "b_pawn";
-        board[1][1] = "b_pawn";
-        board[1][2] = "b_pawn";
-        board[1][3] = "b_pawn";
-        board[1][4] = "b_pawn";
-        board[1][5] = "b_pawn";
-        board[1][6] = "b_pawn";
-        board[1][7] = "b_pawn";
+        board[0][5] = "b_bishop_r";
+        board[0][6] = "b_knight_r";
+        board[0][7] = "b_rook_r";
+        board[1][0] = "b_pawn_1";
+        board[1][1] = "b_pawn_2";
+        board[1][2] = "b_pawn_3";
+        board[1][3] = "b_pawn_4";
+        board[1][4] = "b_pawn_5";
+        board[1][5] = "b_pawn_6";
+        board[1][6] = "b_pawn_7";
+        board[1][7] = "b_pawn_8";
 
         // initialize remaining board with no pieces
         for (int i = 2; i < 6; i++) {
