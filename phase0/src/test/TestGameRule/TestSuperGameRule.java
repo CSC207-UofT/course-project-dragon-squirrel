023bf9fe224823335e8d3027300a499fe16fb0bb
chrisphil335 <61081461+chrisphil335@users.noreply.github.com>
2021-11-15 12:25:22 -0500

    added TestGameRule

--- phase0/src/test/TestGameRule/TestSuperGameRule.java
+++ phase0/src/test/TestGameRule/TestSuperGameRule.java
@@ -1,7 +1,34 @@
 package TestGameRule;
 
+import Board.Board;
+import Board.SuperBoard;
+import Command.MoveRecord;
+import GameRule.SuperGameRule;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+import piece.PieceInterface;
+import Command.MoveRecord;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class TestSuperGameRule {
 
+    Board superBoard;
+    Map<String, PieceInterface> superPiecesDict;
+    MoveRecord mr;
+    SuperGameRule sgr;
 
+    @Before
+    public void before(){
+        superBoard = new Board(13, 10);
+        superPiecesDict = new HashMap<String, PieceInterface>();
+        mr = new MoveRecord();
+        sgr = new SuperGameRule(superBoard, superPiecesDict, mr);
+    }
+
+    @Test
+    public void Test
 
 }
