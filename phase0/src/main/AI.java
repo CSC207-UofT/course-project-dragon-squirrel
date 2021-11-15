92387e91aea2445819bebb7343a4dd429d119c6d
chrisphil335 <61081461+chrisphil335@users.noreply.github.com>
2021-11-15 07:22:21 -0500

    added TestChessMove

--- phase0/src/main/AI.java
+++
@@ -1,8 +0,0 @@
-public class AI extends Player{
-
-    public AI(String ID) {
-        super(ID);
-    }
-
-    // Designing a game (especially board game) AI could be difficult, maybe leave it until phase 2
-}
