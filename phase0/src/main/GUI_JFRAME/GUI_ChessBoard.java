7645fe5d7832d4ba5d6bc9f931e7d9d80e63739f
Tingzhou Gu <tingzhou.gu@mail.utoronto.ca>
2021-11-27 09:32:07 -0500

    small changes

--- phase0/src/main/GUI_JFRAME/GUI_ChessBoard.java
+++ phase0/src/main/GUI_JFRAME/GUI_ChessBoard.java
@@ -5,12 +5,14 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.lang.reflect.MalformedParameterizedTypeException;
+import java.util.Map;
 
 public class GUI_ChessBoard extends JFrame {
     // icons is an array list that store the initial stage of each pieces, note that empty space is considered as an
     // empty piece. We can, if that's the way, by changing the order of the list, to make moves.
 
-    /*
+
     private PieceIcon[] icons = new PieceIcon[]{
             // white
             new PieceIcon("\u2656"), new PieceIcon("\u2658"), new PieceIcon("\u2657"),
@@ -40,7 +42,6 @@ public class GUI_ChessBoard extends JFrame {
             new PieceIcon("\u265C")
     };
 
-     */
 
     // TODO: Probably will change some code below, as we need to have operation on the board
     JButton[] coords_button = new JButton[]{
@@ -62,6 +63,7 @@ public class GUI_ChessBoard extends JFrame {
             new JButton(), new JButton(), new JButton(), new JButton(),
     };
 
+
     String[] coords = new String[]{
             "00", "01", "02", "03", "04", "05", "06", "07",
             "10", "11", "12", "13", "14", "15", "16", "17",
@@ -72,9 +74,11 @@ public class GUI_ChessBoard extends JFrame {
             "60", "61", "62", "63", "64", "65", "66", "67",
             "70", "71", "72", "73", "74", "75", "76", "77",
     };
-    public GUI_ChessBoard(){}
 
-    void display(){
+    public GUI_ChessBoard() {
+    }
+
+    void display() {
         setTitle("Chess board");
 
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -84,42 +88,43 @@ public class GUI_ChessBoard extends JFrame {
         GridLayout gridLayout = new GridLayout(8, 8);
 
         contentPane.setLayout(gridLayout);
-        /*
-        int row = -1;
-        for (int i = 0; i < icons.length; i++)
-        {
-            if(i % 8 == 0) row ++; // increment row number
-            icons[i].set(i, row);
-            contentPane.add(icons[i]);
-        } // i
-        */
+
         int r = -1;
-        for (int j = 0; j < coords_button.length; j++){
-            if(j % 8 == 0) r ++;
+        for (int j = 0; j < coords_button.length; j++) {
+            if (j % 8 == 0) r++;
             coords_button[j].setBounds(j, r, contentPane.getWidth(), contentPane.getHeight());
             coords_button[j].setOpaque(false);
             coords_button[j].addActionListener(new ActionListener() {
                 @Override
                 public void actionPerformed(ActionEvent e) {
-                    for(int w = 0; w < coords_button.length; w++) {
+                    for (int w = 0; w < coords_button.length; w++) {
                         if (e.getSource() == coords_button[w]) {
-                            System.out.println(coords[w]);
+                            // this set up the action listener for each button, but in a for loop
+                            // can add the action listeners to buttons before line 92,
+                            System.out.println(coords[w]); // test
+
                         }
                     }
                 }
             });
             contentPane.add(coords_button[j]);
-        }
 
+        /*
+        # This uses the PieceIcon for the layout
+        int row = -1;
+        for (int i = 0; i < icons.length; i++) {
+            if (i % 8 == 0) row++; // increment row number
+            icons[i].set(i, row);
+            contentPane.add(icons[i]);
+        }
+        */
         setSize(800, 800);
         setLocationRelativeTo(null);
         setVisible(true);
+        }
 
+        // TODO: Allow pieces to move by interacting with other code, idea: by change the array of icons.
 
+        // Here should be some code.
     }
-
-    // TODO: Allow pieces to move by interacting with other code, idea: by change the array of icons.
-
-    // Here should be some code.
-
 }
