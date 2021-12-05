6abf5d0a4a72941c7a28c267d802a46560094c1e
chrisphil335 <61081461+chrisphil335@users.noreply.github.com>
2021-12-05 14:40:52 -0500

    adding knight test

--- phase0/src/test/TestPiece/TestKnight.java
+++ phase0/src/test/TestPiece/TestKnight.java
@@ -8,17 +8,28 @@ import piece.Knight;
 import static org.junit.Assert.assertEquals;
 
 public class TestKnight {
+
     Knight k;
 
     @Before
-    public void setup(){
+    public void before(){
         k = new Knight("w_knight", Color.WHITE);
+    }
+
+    @Test(timeout = 50)
+    public void TestgetValue(){
+        assertEquals(300, k.getValue());
     }
 
     @Test(timeout = 50)
     public void TestvalidMove(){
         assertEquals(true, k.validMove(7, 1, 5, 2));
         assertEquals(false, k.validMove(7, 1, 5, 3));
+    }
+
+    @Test(timeout = 50)
+    public void TestdeepCopy(){
+        assertEquals(k.getName(), k.deepCopy().getName());
     }
 
 }
