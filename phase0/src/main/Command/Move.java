be7bc8ef6556a0ca9977062a7cbc010b54a7235e
jenniferli2002 <jenniferjunru.li@mail.utoronto.ca>
2021-12-07 16:07:15 -0500

    Fix problem in castling

--- phase0/src/main/Command/Move.java
+++ phase0/src/main/Command/Move.java
@@ -1,8 +1,10 @@
 package Command;
 
 import BoardManager.BoardManager;
+import piece.King;
 import piece.Pawn;
 import piece.PieceInterface;
+import piece.Rook;
 
 public abstract class Move implements Command{
     protected BoardManager BM;
@@ -25,12 +27,22 @@ public abstract class Move implements Command{
     public void execute() {
         if (actionPiece instanceof Pawn)
             ((Pawn) actionPiece).hasNotMovedDuringGame = false;
+        if (actionPiece instanceof Rook)
+            ((Rook) actionPiece).setHasMovedDuringGame(true);
+        if (actionPiece instanceof King)
+            ((King) actionPiece).setHasMovedDuringGame(true);
     }
     
     @Override
     public void undo() {
         if (CM.getFirstMoveStatus() && actionPiece instanceof Pawn) {
             ((Pawn) actionPiece).hasNotMovedDuringGame = true;
+        }
+        if (CM.getFirstMoveStatus() && actionPiece instanceof Rook) {
+            ((Rook) actionPiece).setHasMovedDuringGame(false);
+        }
+        if (CM.getFirstMoveStatus() && actionPiece instanceof King) {
+            ((King) actionPiece).setHasMovedDuringGame(false);
         }
     }
 }
